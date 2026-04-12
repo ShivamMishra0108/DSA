9c8b8833a187e714b7e0629f7b95313031f8a0dd
Shivam Mishra <shivammishra6339@gmail.com>
2026-04-12 14:04:00 +0530

    solved lc 560

--- src/Arrays/SubarraySumEqualsK.java
+++ src/Arrays/SubarraySumEqualsK.java
@@ -1,4 +1,20 @@
 package Arrays;
 
 public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+
+        int n = nums.length;
+        int count = 0;
+
+        for(int st=0; st<n; st++ ){
+            int cursum = 0;
+
+            for(int end=st; end<n; end++){
+                cursum  += nums[end];
+
+                if(cursum == k)count++;
+            }
+        }
+        return count;
+    }
 }
--- src/Heap/KPairWithSmallestSum.java
+++ src/Heap/KPairWithSmallestSum.java
@@ -1,42 +1,42 @@
-package Heap;
-
-import java.util.*;
-class triplet implements Comparable<triplet>{
-    int sum;
-    int x;
-    int y;
-
-    triplet(int sum,int x ,int y){
-        this.sum = sum;
-        this.x = x;
-        this.y = y;
-    }
-
-    public int compareTo(triplet t){
-        return Integer.compare(this.sum,t.sum);
-    }
-}
-
-public class KPairWithSmallestSum {
-    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-
-        PriorityQueue<triplet> pq = new PriorityQueue<triplet>();
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=0; i<nums1.length; i++){
-            for(int j=0; j<nums2.length; j++){
-                pq.add(new triplet(nums1[i]+nums2[j],nums1[i],nums2[j]));
-            }
-        }
-        for(int i=0; i<k; i++){
-            triplet top = pq.remove();
-            list.add(top.x);
-            list.add(top.y);
-            ans.add(list);
-            list.clear();
-        }
-        return ans;
-
-    }
-}
+//package Heap;
+//
+//import java.util.*;
+//class triplet implements Comparable<triplet>{
+//    int sum;
+//    int x;
+//    int y;
+//
+//    triplet(int sum,int x ,int y){
+//        this.sum = sum;
+//        this.x = x;
+//        this.y = y;
+//    }
+//
+//    public int compareTo(triplet t){
+//        return Integer.compare(this.sum,t.sum);
+//    }
+//}
+//
+//public class KPairWithSmallestSum {
+//    public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
+//
+//        PriorityQueue<triplet> pq = new PriorityQueue<triplet>();
+//        List<List<Integer>> ans = new ArrayList<>();
+//        List<Integer> list = new ArrayList<>();
+//
+//        for(int i=0; i<nums1.length; i++){
+//            for(int j=0; j<nums2.length; j++){
+//                pq.add(new triplet(nums1[i]+nums2[j],nums1[i],nums2[j]));
+//            }
+//        }
+//        for(int i=0; i<k; i++){
+//            triplet top = pq.remove();
+//            list.add(top.x);
+//            list.add(top.y);
+//            ans.add(list);
+//            list.clear();
+//        }
+//        return ans;
+//
+//    }
+//}
