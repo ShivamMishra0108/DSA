5b0c4a80aa1aeda1748ea0c7a6b9a28395df2f45
Shivam Mishra <shivammishra6339@gmail.com>
2026-04-12 13:55:06 +0530

    Solved lc238

---
+++ src/Arrays/ProductofArrayExceptSelf.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ProductofArrayExceptSelf {
+    public static void main() {
+
+        int[] nums = {-1, 1, 0, -3, 3};
+
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+    public static int[] productExceptSelf(int [] nums) {
+
+        int n = nums.length;
+        int [] ans = new int[n];
+
+        int prefix = 1;
+        int suffix = 1;
+
+        ans[0] = prefix;
+        ans[n-1] = suffix;
+
+
+        for(int i=1; i<n; i++){
+            prefix = prefix*nums[i-1];
+            ans[i] = prefix;
+
+        }
+        for(int i=n-2; i>=0; i--){
+            suffix = suffix*nums[i+1];
+            ans[i] = ans[i]*suffix;
+        }
+
+        return ans;
+    }
+}
