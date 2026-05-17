67b94949d91120b9b2d3d4cefcd2a246f3fbd8fc
Shivam Mishra <shivammishra6339@gmail.com>
2026-05-17 11:22:03 +0530

    "Solved Summary ranges"

--- src/TopInterView150/SummaryRanges.java
+++ src/TopInterView150/SummaryRanges.java
@@ -1,15 +1,39 @@
-//package TopInterView150;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class SummaryRanges {
-//    public List<String> summaryRanges(int[] nums) {
-//
-//        List<String> l = new ArrayList<>();
-//
-//        for(int i=0; i<nums.length; i++){
-//            if(nums[i]+1 = num)
-//        }
-//    }
-//}
+package TopInterView150;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    static void main() {
+        summaryRanges(new int[]{0, 2, 3, 4, 6, 8, 9});
+    }
+    public static List<String> summaryRanges(int[] nums) {
+
+        int n = nums.length;
+        List<String> l = new ArrayList<>();
+
+        if(n==0)return l;
+        int start = nums[0];
+
+        for(int i=0; i<n; i++){
+
+            if(i == n-1 || nums[i+1] != nums[i]+1){
+                int end = nums[i];
+
+                if(start == end){
+                    l.add(String.valueOf(start));
+                }
+
+                else{
+                    l.add(start+"->"+end);
+                }
+
+                if(i != n-1){
+                    start = nums[i+1];
+                }
+            }
+        }
+        return l;
+
+    }
+}
