4b017380e320d23dce6a19125d481fcae5d094c9
Shivam Mishra <shivammishra6339@gmail.com>
2026-07-23 09:33:26 +0530

    Solved LC 1277

---
+++ src/DP_Space_Opt_Tabulation/CountSubmatrices.java
@@ -0,0 +1,19 @@
+package DP_Space_Opt_Tabulation;
+
+public class CountSubmatrices {
+    public int countSquares(int[][] arr) {
+        int count=0;
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(i != 0 && j != 0){
+                    if(arr[i][j] == 1){
+                        arr[i][j] += Math.min(arr[i-1][j-1],Math.min(arr[i-1][j],arr[i][j-1]));
+                    }
+                }
+                count += arr[i][j];
+            }
+        }
+        return count;
+    }
+}
